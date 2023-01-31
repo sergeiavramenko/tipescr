0079551fe840b6d7390f89333d905f8a99fff4e9
Sergei <sergei.avramenko199@yandex.ru>
2023-01-31 14:05:46 +0300

    lesson2

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Todolist } from './Todolist';
+
+function App() {
+  let titl1 = [
+    {ip:"What du you know abaut it"},
+    {ip:"let me ddwwa"}
+  ]   
+  let [tasks, setTasks] = useState([
+    {id: 1,    title: "CSS",  isDone: true},
+    {id: 2,    title: "JS",  isDone: true},
+    {id: 3,    title: "React",  isDone: false},
+    {id: 4,    title: "Redux",  isDone: true},
+    {id: 5,    title: "Redukkx",  isDone: false}
+  ]); 
+
+  function removeTask(id:number) {
+    let filteredTasks= tasks.filter( t => t.id !== id)
+    setTasks(filteredTasks);
+
+  }
+  
+  return (
+    <div className='App'>
+      <Todolist tit={titl1} tasks={tasks}
+      removeTask={removeTask} />
+      
+     
+     
+    
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+
+type Bebby = {
+    ip: string
+}
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+type PropsTyp = {
+    tit: Array<Bebby>
+    tasks: Array<TaskType>
+    removeTask: Function    
+}
+
+export function Todolist(props: PropsTyp) {
+    return (
+      <div>
+        <h3>{props.tit[0].ip}</h3>
+        <div>
+          <input />
+          <button>+</button>
+        </div>
+        <ul>
+            {props.tasks.map((t) => {
+                return <li><input type="checkbox" checked={t.isDone}/> 
+                <span>{t.title}</span>
+                <button onClick={() => {props.removeTask(t.id) }}> x </button>
+                </li> 
+          
+            })
+
+
+            }
+          
+        </ul>
+        <div>
+          <button>all</button>
+          <button>active</button>
+          <button>Completed</button>
+        </div> 
+      </div>
+  
+    );
+  }
